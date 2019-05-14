f99cdd3797f9592bcb7f56107bc6d63ff79c0f31
Mama <mamadembele7@gmail.com>
2019-05-15 00:44:03 +0200

    maj

--- TP2.R
+++ TP2.R
@@ -1,28 +1,56 @@
-#Implémentation algo analyse en composantes principales
-library('plot3D')
-#1
-data = read.table("~/dataAnalysis/data1TP2.txt", header = TRUE)
-scatter3D(data$Stature, data$Poids, data$Taille, xlab = "Stature", ylab = "Poids", zlab = "Taille")
+##################################
+# TP2                            #
+# DEMBELE Mama - DAOUMA Zakaria  #
+#################################
 
-#2
-meanStature = mean(data$Stature)
-meanPoids = mean(data$Poids)
-meanTaille = mean(data$Taille)
+library("plot3D")
+my_data2 <- read.delim("/home/pakendux/dataAnalysis/data1TP2.txt")
+scatter3D(my_data2$Stature, my_data2$Poids, my_data2$Taille, colvar = my_data2$Taille, col = NULL, add = FALSE)
 
-B = cbind(data$Stature - meanStature, data$Poids - meanPoids, data$Taille - meanTaille)
-V = cov(B)
+# question 2
+moyStature <- mean(my_data2$Stature) 
+moyPoids <- mean(my_data2$Poids) 
+moyTaille <- mean(my_data2$Taille) 
+tabStature <- c()
+tabPoids <- c()
+tabTaille <- c()
+
+for (i in 1:length( my_data2$Stature)) {
+  tabStature[i] <- my_data2$Stature[i]- moyStature
+  
+}
+for (i in 1:length( my_data2$Poids)) {
+  tabPoids[i] <- my_data2$Poids[i]- moyPoids
+ 
+}
+for (i in 1:length( my_data2$Taille)) {
+  tabTaille[i] <- my_data2$Taille[i]- moyTaille
+ 
+}
+B<-matrix( c(tabStature, tabPoids, tabTaille), # the data elements 
+  nrow=10,              # number of rows 
+  ncol=3,              # number of columns 
+  byrow = FALSE
+)
+
+print(B)
+V <- cov(B)
 print(V)
+#question 3
+x = eigen(V)
+print("les valeurs propres ")
+print(x$values)
+print(x$vectors)
+#question 4
+# l'axe principale est le premier axe des vecteurs propres 
+#question 5
+C <- B%*%x$vectors
+print(C)
+princomp(my_data2)$scores
+#question 6
+scatter3D(x =c (0,-300*x$vectors[1,1]), y =c(0,-300*x$vectors[2,1]), z = c(0,-300*x$vectors[3,1]), add = TRUE, type ="l")
+plotrgl()
+# question 7
+plot(C[,1],C[,2])
 
-#3
-vvp = eigen(V)
-vectP = vvp$vectors
-valP = vvp$values
-print(vectP)
-print(valP)
-
-#4
-
-#5
-C = B %*% vectP
-princomp(data)$scores
 
